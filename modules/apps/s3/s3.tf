767f728ca212011078c4bac6ab7580733ee5af2a
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2025-01-19 15:01:50 +0900

    add permission

--- modules/apps/s3/s3.tf
+++ modules/apps/s3/s3.tf
@@ -27,7 +27,7 @@ resource "aws_s3_bucket_policy" "website" {
       {
         Effect    = "Allow"
         Principal = "*"
-        Action    = ["s3:GetObject", "s3:ListBucket", "s3:PutObject"]
+        Action    = ["s3:GetObject", "s3:ListBucket", "s3:PutObject", "s3:DeleteObject"]
         Resource  = "${aws_s3_bucket.website.arn}/*"
       },
     ]
