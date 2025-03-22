5d72eaac3102bbc1b753e205e0f7230aa487ccb3
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2025-03-22 17:52:09 +0900

    delete file

--- modules/apps/lambda/src/lambda_function.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    print("The object is uploaded to S3 buckets")
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
