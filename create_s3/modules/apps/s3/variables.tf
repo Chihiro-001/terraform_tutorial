bfbba4c8c274dfa7a25d157e58df7d4c91128603
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2025-03-26 22:16:25 +0900

    update aws_security_group_rule

--- create_s3/modules/apps/s3/variables.tf
+++ create_s3/modules/apps/s3/variables.tf
@@ -3,15 +3,15 @@ variable "config" {
 }
 
 variable "env" {
-  
+
 }
 
 variable "lambda_function_arn" {
   description = "The ARN of the lambda function"
-  
+
 }
 
 variable "lambda_permission_id" {
   description = "The ID of the lambda permission"
-  
+
 }
--- intro_awscc/environment/stg/main.tf
+++ intro_awscc/environment/stg/main.tf
@@ -1,4 +1,4 @@
 module "main" {
-  source = "../../../modules/main"
-  
+  source = "../../modules"
+
 }
