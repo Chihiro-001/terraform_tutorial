67c6cc6d61e132686445173e36db8b904d151c9b
Chihiro-001 <98835638+Chihiro-001@users.noreply.github.com>
2025-03-26 22:17:26 +0900

    update aws_security_group_rule

--- intro_awscc/modules/main.tf
+++ intro_awscc/modules/main.tf
@@ -1,32 +1,15 @@
 # AWSCC
 resource "awscc_ec2_security_group_egress" "example" {
-  group_id    = awscc_ec2_security_group.allow_all.id
+  group_id    = aws_security_group.allow_all.id
   ip_protocol = "tcp"
   from_port   = 80
   to_port     = 80
   cidr_ip     = "0.0.0.0/0"
-  description = "Allow HTTP traffic"
-
+  description = "Created by AWSCC Provider"
   lifecycle {
     prevent_destroy = true
   }
 }
-
-# resource "awscc_ec2_security_group" "allow_all" {
-#   group_description = "Allow all outbound traffic"
-#   vpc_id            = awscc_ec2_vpc.selected.id
-
-#   tags = [
-#     {
-#       key   = "Name"
-#       value = "allow_all"
-#     }
-#   ]
-# }
-
-# resource "awscc_ec2_vpc" "selected" {
-#   cidr_block = "10.0.0.0/16"
-# }
 
 # ---
 
@@ -46,18 +29,18 @@ resource "aws_security_group" "allow_all" {
   vpc_id      = aws_vpc.selected.id
 
   tags = {
-    Name = "allow_all"
+    Name = "hands_on"
   }
 }
 
-# resource "aws_security_group_rule" "example" {
-#   type              = "egress"
-#   security_group_id = aws_security_group.allow_all.id
-#   protocol          = "tcp"
-#   from_port         = 80
-#   to_port           = 80
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   description       = "Allow HTTP traffic"
+resource "aws_security_group_rule" "example" {
+  type              = "egress"
+  security_group_id = aws_security_group.allow_all.id
+  protocol          = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Created by AWS Provider"
 
-# }
+}
 
